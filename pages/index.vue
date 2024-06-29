23e1963ed57ce382dd36eb2f67f820a8dd279a2e
Jo <jojostev@gmail.com>
2024-06-29 18:06:49 +0400

    add some old untracked files

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Welcome to My Nuxt App</h1>
+      <p>This is the home page.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  // Any specific logic for the index page can go here
+  </script>
+  
+  <style scoped>
+  /* Add any scoped styles for the index page here */
+  </style>
+  
